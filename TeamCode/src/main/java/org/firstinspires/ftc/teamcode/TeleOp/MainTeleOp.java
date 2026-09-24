cb5ad6bc27fb18da87bc4b3d11c2687d303a1173
Ryan Z <zoogazooga6688@gmail.com>
2026-09-24 14:27:55 -0400

    Cannon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleOp.java
@@ -7,50 +7,34 @@ import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp(name = "Main TeleOp", group = "Teleop")
 public class MainTeleOp extends LinearOpMode {
-    public static MotorEx motor;
+    public DcMotorEx motor;
+
+    public static double speed;
     public static GamepadEx gp1;
 
     public void runOpMode() throws InterruptedException{
-        motor = new MotorEx(this.hardwareMap, "IntakeMotor", Motor.GoBILDA.RPM_1150);
+        motor = hardwareMap.get(DcMotorEx.class, "Cannon");
         gp1 = new GamepadEx(gamepad1);
 
         waitForStart();
         while (opModeIsActive()){
-            if (gp1.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER) > 0.1) {
-                motor.set(1);
-            }
-            else if (gp1.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.1) {
-                motor.set(-1);
+
+            if (gp1.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.1) {
+                motor.setVelocity(speed*360, AngleUnit.DEGREES);
             }
             else{
-                motor.set(0);
+                motor.setVelocity(0);
             }
 
         }
 
     }
-    public void driveRobotCentric(double strafeSpeed, double forwardBackSpeed, double turnSpeed) {
-        double[] speeds = {
-                (forwardBackSpeed + strafeSpeed - turnSpeed),
-                (forwardBackSpeed - strafeSpeed + turnSpeed),
-                (forwardBackSpeed - strafeSpeed - turnSpeed),
-                (forwardBackSpeed + strafeSpeed + turnSpeed)
-        };
-        double maxSpeed = 0;
-        for (int i = 0; i < 4; i++) {
-            maxSpeed = Math.max(maxSpeed, abs(speeds[i]));
-        }
-        if (maxSpeed > 1) {
-            for (int i = 0; i < 4; i++) {
-                speeds[i] /= maxSpeed;
-            }
-        }
-        fl.set(speeds[0]);
-        fr.set(speeds[1]);
-        bl.set(speeds[2]);
-        br.set(speeds[3]);
-    }
+
 }
